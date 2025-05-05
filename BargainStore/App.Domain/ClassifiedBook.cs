54bed13fe25612acd10c7f3ad3f9c85c36ee2ca9
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-05-05 10:09:47 +0000

    Added first rich entity class

---
+++ BargainStore/App.Domain/ClassifiedBook.cs
@@ -0,0 +1,30 @@
+﻿namespace App.Domain;
+
+public class ClassifiedBook
+{
+    public Guid Id { get; private set; }
+
+    public ClassifiedBook(Guid id, Guid ownerId)
+    {
+        if (id == default)
+        {
+            throw new ArgumentException("Identity must be specified", nameof(id));
+        }
+        if (ownerId == default)
+        {
+            throw new ArgumentException("Owner Id must be specified", nameof(ownerId));
+        }
+        Id = id;
+    }
+
+    // Behaviors
+    public void SetTitle(string title) => _title = title;
+    public void UpdateDetails(string details) => _details = details;
+    public void UpdateListPrice(decimal listPrice) => _listPrice = listPrice;
+
+    private Guid _ownerId;
+    private string _title;
+    private string _details;
+    private decimal _listPrice;
+
+}
