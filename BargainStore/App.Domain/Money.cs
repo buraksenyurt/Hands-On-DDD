ac4014cc22bf12c64bb281f5a7cc5d15b4dd03e7
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2025-05-05 14:03:23 +0000

    Enriched the money type.

--- BargainStore/App.Domain/Money.cs
+++ BargainStore/App.Domain/Money.cs
@@ -1,7 +1,23 @@
 namespace App.Domain;
 
-public record Money(decimal Amount)
+public record Money
 {
+    private const string DefaultCurrencyCode = "TL";
+    public decimal Amount { get; }
+    public string CurrencyCode { get; }
+    protected Money(decimal amount, string currencyCode = "TL")
+    {
+        Amount = amount;
+        CurrencyCode = currencyCode;
+    }
+    public static Money FromDecimal(decimal amount, string currencyCode = DefaultCurrencyCode)
+    {
+        return new Money(amount, currencyCode);
+    }
+    public static Money FromString(string amount, string currencyCode = DefaultCurrencyCode)
+    {
+        return new Money(decimal.Parse(amount), currencyCode);
+    }
     public static Money operator +(Money summand1, Money summand2)
     {
         return summand1.Add(summand2);
@@ -12,19 +28,23 @@ public record Money(decimal Amount)
     }
     public Money Add(Money summand)
     {
+        if (CurrencyCode != summand.CurrencyCode)
+        {
+            throw new MismatchedCurrencyCodeException("Can not add amounts with different currencies");
+        }
         return new Money(Amount + summand.Amount);
     }
     public Money Substract(Money subtrahend)
     {
+        if (CurrencyCode != subtrahend.CurrencyCode)
+        {
+            throw new MismatchedCurrencyCodeException("Can not subtract amounts with different currencies");
+        }
         return new Money(Amount - subtrahend.Amount);
     }
-    public static Money FromDecimal(decimal amount)
+    public class MismatchedCurrencyCodeException(string message)
+        : Exception(message)
     {
-        return new Money(amount);
-    }
-    public static Money FromString(string amount)
-    {
-        return new Money(decimal.Parse(amount));
     }
 }
 
--- BargainStore/App.Tests/MoneyInstanceTests.cs
+++ BargainStore/App.Tests/MoneyInstanceTests.cs
@@ -7,25 +7,25 @@ public class MoneyInstanceTests
     [Fact]
     public void Money_instances_with_the_same_amount_should_be_equal()
     {
-        var leftAmount = new Money(49.99M);
-        var rightAmount = new Money(49.99M);
+        var leftAmount = Money.FromDecimal(49.99M);
+        var rightAmount = Money.FromDecimal(49.99M);
         Assert.Equal(leftAmount, rightAmount);
     }
     [Fact]
     public void Sum_of_money_gives_right_amount()
     {
-        var amount1 = new Money(12.50M);
-        var amount2 = new Money(12.50M);
-        var expected = new Money(25.00M);
+        var amount1 = Money.FromDecimal(12.50M);
+        var amount2 = Money.FromDecimal(12.50M);
+        var expected = Money.FromDecimal(25.00M);
         var actual = amount1 + amount2;
         Assert.Equal(expected, actual);
     }
     [Fact]
     public void Subtrahend_of_money_gives_right_amount()
     {
-        var amount1 = new Money(12.50M);
-        var amount2 = new Money(2.50M);
-        var expected = new Money(10.00M);
+        var amount1 = Money.FromDecimal(12.50M);
+        var amount2 = Money.FromDecimal(2.50M);
+        var expected = Money.FromDecimal(10.00M);
         var actual = amount1 - amount2;
         Assert.Equal(expected, actual);
     }
@@ -33,8 +33,15 @@ public class MoneyInstanceTests
     public void Create_money_instance_from_string_should_works()
     {
         var amount = "34.55";
-        var expected = new Money(34.55M);
+        var expected = Money.FromDecimal(34.55M);
         var actual = Money.FromString(amount);
         Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Sum_of_money_with_different_currency_should_raise_an_exception()
+    {
+        var amount1 = Money.FromDecimal(34.55M, "USD");
+        var amount2 = Money.FromDecimal(350.00M);
+        Assert.Throws<Money.MismatchedCurrencyCodeException>(() => amount1 + amount2);
     }
 }
